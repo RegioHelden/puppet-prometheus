c0108c10aa986574f7ae5abea37f16ca90c32548
Antoine Beaupré <anarcat@debian.org>
2019-03-18 21:50:39 -0400

    set node-exporter job name to "node"
    This is the common configuration and diverging from that upsets
    Grafana defaults and manual configurations. It's better to stick with
    a known name than make a new random one. It's also useful to be able
    to tell the daemon which job name to export.

--- manifests/daemon.pp
+++ manifests/daemon.pp
@@ -84,6 +84,7 @@ define prometheus::daemon (
   Boolean $export_scrape_job      = false,
   Stdlib::Fqdn $scrape_host       = $facts['fqdn'],
   Optional[Stdlib::Port] $scrape_port = undef,
+  String $scrape_job_name         = $name,
 ) {
 
   case $install_method {
@@ -260,7 +261,7 @@ define prometheus::daemon (
     }
 
     @@prometheus::scrape_job { "${scrape_host}:${scrape_port}":
-      job_name => $name,
+      job_name => $scrape_job_name,
       targets  => ["${scrape_host}:${scrape_port}"],
     }
   }
